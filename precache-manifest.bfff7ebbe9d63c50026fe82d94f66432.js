4efc9cb825d857b0197d51dcd50819b48c2d50e1
spunkiee <ramashishkhush@gmail.com>
2020-09-16 21:33:30 +0530

    Updates

---
+++ precache-manifest.bfff7ebbe9d63c50026fe82d94f66432.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1e060824b880ff1bc75a9d93c62e0e9",
+    "url": "/survey-tiger/index.html"
+  },
+  {
+    "revision": "104ce4efe882011fd931",
+    "url": "/survey-tiger/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "3ca7c8223ba518e984e8",
+    "url": "/survey-tiger/static/css/main.f0ec89f5.chunk.css"
+  },
+  {
+    "revision": "104ce4efe882011fd931",
+    "url": "/survey-tiger/static/js/2.3ad06fd9.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/survey-tiger/static/js/2.3ad06fd9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ca7c8223ba518e984e8",
+    "url": "/survey-tiger/static/js/main.ebcd7f3e.chunk.js"
+  },
+  {
+    "revision": "ae290f7472aab3c980f7",
+    "url": "/survey-tiger/static/js/runtime-main.6859db03.js"
+  }
+]);
